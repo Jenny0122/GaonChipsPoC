df0b81436090ec8ed2758223b0c652383ba31fa2
안선정 <asj4459@naver.com>
2023-12-18 12:20:45 +0900

    First Upload

---
+++ GaonChipsPoC/src/test/java/com/wisenut/GaonChipsPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisenut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaonChipsPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
