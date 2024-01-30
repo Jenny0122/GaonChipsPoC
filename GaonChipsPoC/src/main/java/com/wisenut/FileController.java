4c04849d224a17c82119130866f59344db2919d2
안선정 <asj4459@naver.com>
2024-01-30 22:39:12 +0900

    .

--- GaonChipsPoC/src/main/java/com/wisenut/FileController.java
+++ GaonChipsPoC/src/main/java/com/wisenut/FileController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -69,7 +68,10 @@ public class FileController {
 
 		return ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION,
-						"attachment; filename=\"" + UriUtils.encode(name, "UTF-8") + "\"")
+						"attachment; filename=\""
+//				+ UriUtils.encode(name, "UTF-8")
+				+ name
+				+ "\"")
 				.body(resource);
 	}
 
